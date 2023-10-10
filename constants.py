42b0ac0981eac829e900a6c439da1e21937b049d
Mathieu Vavrille (Swing) <mathieu.vavrille@gmail.com>
2023-10-10 13:21:21 +0200

    I forgor

---
+++ constants.py
@@ -0,0 +1,7 @@
+
+
+LINEWIDTH_PROPORTION = 1/5 # linewidth as a proportion of the base scale
+
+DOT_PROPORTION = 1/10 # proportion of the base scale associated to dot plotting
+INTERESTING_DOT_SCALING = 2 # when highlighting a dot, scales the DOT_PROPORTION by this value
+PLAYER_DOT_SCALING = 3 # when highlighting a dot, scales the DOT_PROPORTION by this value
